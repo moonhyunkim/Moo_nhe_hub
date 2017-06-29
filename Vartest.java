8787d765f89e842f28266b0856b7e6f302186791
김문현 <rlaansgus88@gmail.com>
2017-06-29 21:35:28 +0900

    안정적인 작업2

--- Vartest.java
+++ Vartest.java
@@ -7,8 +7,8 @@ public class Vartest {
 		System.out.println("---------------");
 
 	}
-		System.out,ptintln("Nomal Task1);
-		System.out.println();
+		System.out,ptintln("Nomal Task1");
+		System.out.println("Nomal Task2");
 	public static void main(String[] args) {
 		Vartest vt = new Vartest();
 		vt.argTest(100, 200, 300, 400);
