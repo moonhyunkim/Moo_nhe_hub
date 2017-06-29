d68e1253dd90fe280e6794472ec80c4c602ff30e
김문현 <rlaansgus88@gmail.com>
2017-06-29 18:51:42 +0900

    java 파일 추가

---
+++ Vartest.java
@@ -0,0 +1,18 @@
+package Practice0628;
+
+public class Vartest {
+	public void argTest(int... n) {
+		for (int i = 0; i < n.length; i++)
+			System.out.println("n[" + i + "]" + n[i]);
+		System.out.println("---------------");
+
+	}
+
+	public static void main(String[] args) {
+		Vartest vt = new Vartest();
+		vt.argTest(100, 200, 300, 400);
+		vt.argTest(100, 200);	
+
+	}
+
+}
